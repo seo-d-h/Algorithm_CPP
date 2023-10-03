8f37c1efd7bc2ebbf179f21a26af494842de32a6
DongHyun-Seo <103934004+seo-d-h@users.noreply.github.com>
2023-10-03 10:26:03 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+    cin >> n;
+
+    for(int i=0;i<n;i++){
+        for(int j=0;j<i;j++){
+            cout << ' ';
+        }
+        for(int j=0;j<n-i;j++){
+            cout << '*';
+        }
+        cout << "\n";
+    }
+}
