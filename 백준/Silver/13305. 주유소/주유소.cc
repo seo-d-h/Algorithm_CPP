feab5471e897936518dcbbf9288a1ea71e8d30cc
DongHyun-Seo <103934004+seo-d-h@users.noreply.github.com>
2023-10-20 19:31:32 +0900

    [Silver III] Title: 주유소, Time: 24 ms, Memory: 3460 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -1,15 +1,18 @@
 #include <iostream>
 #include <limits.h>
 using namespace std;
+#define ll long long
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
     
-    int N, res=0;
-    int mini = INT_MAX;
-    int cost[100002];
-    int dist[100002];
+    int N;
+    ll res=0;
+    ll mini = LONG_LONG_MAX;
+    ll cost[100002];
+    ll dist[100002];
+
     cin >> N;
 
     for(int i=0;i<N-1;i++){
@@ -19,9 +22,9 @@ int main(){
     for(int i=0;i<N;i++){
         cin >> cost[i];
     }
-    mini = cost[0];
-    res += cost[0]*dist[0];
-    for(int i=1;i<=N;i++){
+    // mini = cost[0];
+    // res += cost[0]*dist[0];
+    for(int i=0;i<N-1;i++){
         mini = min(mini, cost[i]);
         res += mini * dist[i];
     }
